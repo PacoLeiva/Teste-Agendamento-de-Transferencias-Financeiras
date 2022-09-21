c101345a2dc49a7dca86a5572b6c1f753f437f83
PacoLeiva <pacoleiva89@gmail.com>
2022-09-21 09:55:37 -0300

    Add files via upload

---
+++ franciscoleiva-teste/src/main/java/com/franciscoleiva/teste/FranciscoleivaTesteApplication.java
@@ -0,0 +1,13 @@
+package com.franciscoleiva.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranciscoleivaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranciscoleivaTesteApplication.class, args);
+	}
+
+}
---
+++ franciscoleiva-teste/src/test/java/com/franciscoleiva/teste/FranciscoleivaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoleiva.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranciscoleivaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
